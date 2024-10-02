7c443e8774569673f9fd83596b3b40ccfa9f1fea
AZIZEM81 <eaziz428@gmail.com>
2024-10-02 23:05:07 +0000

    azizjspromise06

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,12 @@
-import asyncUploadUser from "./100-await";
-
-const test = async () => {
-    const value = await asyncUploadUser();
-    console.log(value);
-};
-
-test();
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(String(error));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
