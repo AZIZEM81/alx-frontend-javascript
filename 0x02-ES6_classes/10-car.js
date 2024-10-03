6d695583467b55f698313a83b4f09f91007ecaab
AZIZEM81 <eaziz428@gmail.com>
2024-10-03 18:57:26 +0000

    azizclassesjs

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    const Constructor = this.constructor[Symbol.species] || this.constructor;
+    return new Constructor();
+  }
+}
---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+import Car from './10-car.js';
+
+const _range = Symbol('range');
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this[_range] = range;
+  }
+
+  cloneCar() {
+    const Clone = super.constructor[Symbol.species] || super.constructor;
+    return new Clone();
+  }
+}
