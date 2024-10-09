a47349dd83ff6dfba8da9e2f28af8925e5be81fc
AZIZEM81 <eaziz428@gmail.com>
2024-10-09 22:54:03 +0000

    aziz

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,13 +2,16 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+    weakMap.set(endpoint, 1);
+  } else {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   }
-  
-  const count = weakMap.get(endpoint) + 1;
-  weakMap.set(endpoint, count);
-  
+
+  const count = weakMap.get(endpoint);
+
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
+
+  return count;
 }
